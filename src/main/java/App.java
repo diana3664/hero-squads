c0cba3960c7acd11ef76d53fd616548108435c7d
diana3664 <dianamus2017@gmail.com>
2021-06-05 18:26:16 +0300

    add form to collect user data imput

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,7 +10,7 @@ public class App {
     public static void main(String[] args) {
 
 
-        get("/form", (request, response) -> {
+        get("/heroes", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "form.hbs");
 
