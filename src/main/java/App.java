59e61ff593c7050a5193b23cc33a7384035bfa86
diana3664 <dianamus2017@gmail.com>
2021-06-05 18:20:15 +0300

    add a form to collect user input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,20 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+
 public class App {
     public static void main(String[] args) {
+
+
+        get("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "form.hbs");
+
+        }, new HandlebarsTemplateEngine());
 
     }
 }
