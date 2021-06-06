ba66c86478de9d967c0c85c57f992c5a3f8b38d1
diana3664 <dianamus2017@gmail.com>
2021-06-06 17:27:57 +0300

    add a constructor for our squad

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,2 +1,18 @@
+
+
+
 public class Squad {
+
+    private  String mName;
+    private String mCauseForFighting;
+    private String mSize;
+
+    public Squad(String name ,String causeForFighting, String size) {
+        this.mName = name;
+        this.mCauseForFighting = causeForFighting;
+        this.mSize = size;
+
+    }
+
+
 }
