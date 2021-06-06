de1ee8b2f2d7e092705d411da2705c572ad68c04
diana3664 <dianamus2017@gmail.com>
2021-06-06 11:27:36 +0300

    add a constructor for Heros class

--- src/main/java/Heros.java
+++ src/main/java/Heros.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Heros {
+
+    private String mName;
+    private String mAge;
+    private String mPower;
+    private String mWeakness;
+    private static List<Heros> instances = new ArrayList<Heros>();
+    private int mId ;
+
+    public Heros (String name, String age, String power, String weakness){
+        mName = name;
+        mAge =  age;
+        mPower = power;
+        mWeakness = weakness;
+        instances.add(this);
+        mId = instances.size();
+
+    }
+
+
+
 }
