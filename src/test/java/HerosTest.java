0fa2382d81edfbdfc73fc7e9e27deea9cd11d77d
diana3664 <dianamus2017@gmail.com>
2021-06-06 12:16:15 +0300

    add a test instance of heros

--- src/test/java/HerosTest.java
+++ src/test/java/HerosTest.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 public class HerosTest {
+
+    @Test
+    public void newHeros_instantntiateCorrectly() {
+        Heros hero = new Heros("Moh","21","flash","time");
+        assertTrue(hero instanceof Heros);
+
+    }
 }
