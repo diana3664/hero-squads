b88af556f101b645db47f6694590294e4dcf7965
diana3664 <dianamus2017@gmail.com>
2021-06-08 18:20:05 +0300

    add Styling to our headers

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,9 +9,14 @@ import static spark.Spark.get;
 import static spark.Spark.*;
 
 public class App {
+
+
     public static void main(String[] args) {
 
         staticFileLocation("/public");
+
+
+
 
         get("/home", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
