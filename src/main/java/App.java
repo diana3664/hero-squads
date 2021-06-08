d95c10ada76096457d0ff665ed097e31c873648e
diana3664 <dianamus2017@gmail.com>
2021-06-08 23:32:45 +0300

    add final commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,8 +8,13 @@ import static spark.Spark.get;
 import static spark.Spark.*;
 
 public class App {
+
+
     public static void main(String[] args) {
 
+
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
 
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
@@ -74,4 +79,14 @@ public class App {
         }, new HandlebarsTemplateEngine());
 
     }
+
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 }
