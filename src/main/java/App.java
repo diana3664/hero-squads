919214adafde8b926f66a709eff4934a9917ac24
diana3664 <dianamus2017@gmail.com>
2021-06-08 18:58:58 +0300

    adding project name to our pom.xml file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,7 +15,8 @@ public class App {
 
         staticFileLocation("/public");
 
-
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
 
 
         get("/home", (request, response) -> {
@@ -90,6 +91,17 @@ public class App {
 
 
     }
+
+
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 
 
 }
